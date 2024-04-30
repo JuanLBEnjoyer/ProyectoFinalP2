5b23047ffa70ad8d8e2940b42d87341ec94918dc
juanpe23 <juanp.galeanoc@uqvirtual.edu.co>
2024-04-29 20:04:51 -0500

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module co.edu.uniquindio.proyectofinal {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens co.edu.uniquindio.proyectofinal to javafx.fxml;
+    exports co.edu.uniquindio.proyectofinal;
+}
