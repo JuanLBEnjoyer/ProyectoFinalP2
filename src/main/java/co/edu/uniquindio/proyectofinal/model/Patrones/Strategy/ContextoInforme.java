0bb69fb4085149d5318cdf60cbe34716f5272f0a
juanpe23 <juanp.galeanoc@uqvirtual.edu.co>
2024-05-25 22:26:28 -0500

    dcm

--- src/main/java/co/edu/uniquindio/proyectofinal/model/Patrones/Strategy/ContextoInforme.java
+++ src/main/java/co/edu/uniquindio/proyectofinal/model/Patrones/Strategy/ContextoInforme.java
@@ -2,14 +2,27 @@ package co.edu.uniquindio.proyectofinal.model.Patrones.Strategy;
 
 import co.edu.uniquindio.proyectofinal.model.Almacenar.HistorialMedico;
 
+/**
+ * Clase ContextoInforme que maneja la estrategia para generar informes de historial médico.
+ */
 public class ContextoInforme {
 
     private InformeStrategy estrategia;
 
+    /**
+     * Establece la estrategia a utilizar para generar el informe.
+     * 
+     * @param estrategia La estrategia a utilizar.
+     */
     public void setEstrategia(InformeStrategy estrategia) {
         this.estrategia = estrategia;
     }
 
+    /**
+     * Genera un informe del historial médico utilizando la estrategia establecida.
+     * 
+     * @param historial El historial médico del cual se generará el informe.
+     */
     public void generarInforme(HistorialMedico historial) {
         estrategia.generarInforme(historial);
     }
