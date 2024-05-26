b21e7d91f2ce1a6ebe1d4df09f981416b389e978
Mateo-Gomez-Marulanda <mateo.gomezm@uqvirtual.edu.co>
2024-05-26 15:01:21 -0500

    update

---
+++ src/main/java/co/edu/uniquindio/proyectofinal/model/Patrones/Facade/FachadaConsultorio.java
@@ -0,0 +1,5 @@
+package co.edu.uniquindio.proyectofinal.model.Patrones.Facade;
+
+public class FachadaConsultorio {
+    
+}
--- src/main/java/co/edu/uniquindio/proyectofinal/model/Patrones/Fecade/FachadaConsultorio.java
+++
@@ -1,5 +0,0 @@
-package co.edu.uniquindio.proyectofinal.model.Patrones.Fecade;
-
-public class FachadaConsultorio {
-    
-}
